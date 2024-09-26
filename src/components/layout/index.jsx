63652db3a7fa903050416f0ae48cb3918df2b9b0
NIls <alberthxen@gmail.com>
2024-09-26 11:45:55 -0500

    SEO palabra claves

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,13 +1,37 @@
+import { Helmet } from "react-helmet";
 import Header from "../Header";
 import Footer from "../Footer";
-const Layout = ({ children }) => {
+
+const Layout = ({ children, title, description, keywords }) => {
   return (
     <>
-      <Header />
+      <Helmet>
+        <title>{title ? title : "Concresur "}</title>
+        <meta
+          name="description"
+          content={
+            description
+              ? description
+              : "En Concresur ofrecemos soluciones integrales y eficientes en concreto premezclado y prefabricados. Nos destacamos por nuestro servicio personalizado y asesoría técnica especializada para garantizar el éxito y la seguridad en cada proyecto."
+          }
+        />
 
+        <meta
+          name="keywords"
+          content={
+            keywords
+              ? keywords.join(", ")
+              : "Concreto premezclado ,Prefabricados de concreto,Soluciones en concreto,Servicio personalizado,,Asesoría técnica especializada,Calidad en construcción,Proyectos de concreto,Seguridad en proyectos,Concreto eficiente,Construcción con concreto , concresur peru , concresur "
+          }
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+
+      <Header />
       <div>{children}</div>
       <Footer />
     </>
   );
 };
+
 export default Layout;
